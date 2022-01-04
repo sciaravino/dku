a081d57525e0703a51a046dfe65947cd7e64366c
admin <admin>
2022-01-04 09:48:54 -0500

    Saved recipe: LEND.compute_set_date_1

--- recipes/compute_set_date_1.py
+++ recipes/compute_set_date_1.py
@@ -4,20 +4,20 @@ import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
 # Read recipe inputs
-full_data = dataiku.Dataset("full_data")
-full_data_df = full_data.get_dataframe()
+data = dataiku.Dataset("Lending_Club_Scoring")
+df = data.get_dataframe()
 
 #add the start of the month so date can be parsed in future preparation and feature engineering
-full_data_df['earliest_cr_line'] = full_data_df['earliest_cr_line'] + '-01'
-full_data_df.head()
+df['earliest_cr_line'] = df['earliest_cr_line'] + '-01'
+df.head()
 
 # Compute recipe outputs from inputs
 # TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
 # NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
 
-set_date_df = full_data_df # For this sample code, simply copy input to output
+score_date_df = df # For this sample code, simply copy input to output
 
 
 # Write recipe outputs
-set_date = dataiku.Dataset("set_date")
-set_date.write_with_schema(set_date_df)
+score_date = dataiku.Dataset("score_date")
+score_date.write_with_schema(score_date_df)
